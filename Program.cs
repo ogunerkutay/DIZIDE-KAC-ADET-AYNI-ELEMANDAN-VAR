e8d81f8788580a5857199d3c8ed317232b1611e5
OGÜN ERKUTAY <77350593+ogunerkutay@users.noreply.github.com>
2023-02-01 18:50:36 +0300

    Example Linq Query to count items on an array.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIZIDE_KAC_ADET_AYNI_ELEMANDAN_VAR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] cities = new string[]
+                {
+                "İstanbul","Ankara","Bursa","İzmir","Ankara","Samsun","Ordu","Muğla","Antalya","Ankara","İstanbul","Bursa","Çanakkale"
+                };
+            String searchCity = String.Empty;
+            Console.WriteLine("Arayacağınız Şehir Bilgisini Giriniz: ");
+            searchCity = Console.ReadLine();
+
+            Console.WriteLine(cities.Count(sehir => sehir == searchCity));
+
+
+        }
+    }
+}
